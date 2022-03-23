ea3a361d42ef7cb9e74157bfdae3bc4972c8ea60
nvporter <nvporterhouse@gmail.com>
2022-03-23 11:47:52 -0600

    mouse input for menu

--- Game/Casting/Button.cs
+++ Game/Casting/Button.cs
@@ -1,5 +1,34 @@
 namespace black_friday.Game.Casting{
     class Button : Actor{
+
+        public bool CanClick()
+    {
+        Vector2 mousePos = new Vector2(Raylib.GetMousePosition().X, Raylib.GetMousePosition().Y);           // Get the position of the mouse
+        if (mousePos.X >= GlobalPosition.X && mousePos.X <= (GlobalPosition.X + ButtonWidth))
+        {
+            if (mousePos.Y >= GlobalPosition.Y && mousePos.Y <= GlobalPosition.Y + ButtonHeight)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void DetectClick()
+    {
+        if(Raylib.IsMouseButtonDown(MouseButton.MOUSE_LEFT_BUTTON))
+        {
+            if(CanClick())
+            {
+                Console.WriteLine(objectName + " Pos: " + GlobalPosition.ToString());
+                if (onClickAction != null)
+                {
+                    onClickAction();
+                }
+            }
+        }
+    }
         
         public Button(){
             
