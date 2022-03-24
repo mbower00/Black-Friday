05b71d8ae3620b290fe86f58068a1995251465c1
nvporter <nvporterhouse@gmail.com>
2022-03-24 12:27:25 -0600

    button done

--- Game/Casting/Button.cs
+++ Game/Casting/Button.cs
@@ -4,12 +4,13 @@ namespace black_friday.Game.Casting{
     class Button : Actor{
 
         bool isClicked;
-        
         public Button(){
-            
-            
-            
-
+        }
+        public bool GetisClicked(){
+            return isClicked;
+        }
+        public bool SetisClicked(){
+            this.isClicked = isClicked;
         }
     }
 }
