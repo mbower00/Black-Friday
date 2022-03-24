6b4ddea4e1fba82401992adb133c08202df2926e
nvporter <nvporterhouse@gmail.com>
2022-03-24 12:10:18 -0600

    mouse corrected and button

--- Game/Casting/Button.cs
+++ Game/Casting/Button.cs
@@ -1,40 +1,13 @@
-#include "raylib.h"
+//#include "raylib.h"
 using Raylib_cs;
 namespace black_friday.Game.Casting{
     class Button : Actor{
 
         bool isClicked;
         
-        public bool CanClick()
-    {
-        Vector2 mousePos = new Vector2(Raylib.GetMousePosition().X, Raylib.GetMousePosition().Y);           // Get the position of the mouse
-        if (mousePos.X >= GlobalPosition.X && mousePos.X <= (GlobalPosition.X + ButtonWidth))
-        {
-            if (mousePos.Y >= GlobalPosition.Y && mousePos.Y <= GlobalPosition.Y + ButtonHeight)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public void DetectClick()
-    {
-        if(Raylib.IsMouseButtonDown(MouseButton.MOUSE_LEFT_BUTTON))
-        {
-            if(CanClick())
-            {
-                Console.WriteLine(objectName + " Pos: " + GlobalPosition.ToString());
-                if (!isClicked)
-                {
-                    isClicked = true;
-                }
-            }
-        }
-    }
-        
         public Button(){
+            
+            
             
 
         }
--- Game/Services/MouseServices.cs
+++ Game/Services/MouseServices.cs
@@ -1,38 +1,53 @@
+using System.Collections.Generic;
+using Raylib_cs;
 using black_friday.Game.Casting;
 
 
 namespace black_friday.Game.Services
 {
-    public interface MouseService
+    public class RaylibMouseService
     {
-        /// <summary>
-        /// Gets the current mouse cursor coordinates.
-        /// </summary>
-        /// <returns>The mouse cursor coordinates.</returns>
-        Point GetCoordinates();
+        private Dictionary<string, Raylib_cs.MouseButton> buttons
+                = new Dictionary<string, Raylib_cs.MouseButton>() {
+            { "left", Raylib_cs.MouseButton.MOUSE_LEFT_BUTTON },
+            { "middle", Raylib_cs.MouseButton.MOUSE_MIDDLE_BUTTON },
+            { "right", Raylib_cs.MouseButton.MOUSE_RIGHT_BUTTON }
+        };
 
-        /// <summary>
-        /// Whether or not the given button is down.
-        /// </summary>
-        /// <param name="button">The given key.</param>
-        bool IsButtonDown(string button);
+        /// </inheritdoc>
+        public Point GetCoordinates()
+        {
+            int x = Raylib.GetMouseX();
+            int y = Raylib.GetMouseY();
+            return new Point(x, y);
+        }
 
-        /// <summary>
-        /// Whether or not the given button has been pressed.
-        /// </summary>
-        /// <param name="button">The given button.</param>
-        bool IsButtonPressed(string button);
+        /// </inheritdoc>
+        public bool IsButtonDown(string button)
+        {
+            Raylib_cs.MouseButton raylibButton = buttons[button.ToLower()];
+            return Raylib.IsMouseButtonDown(raylibButton);
+        }
 
-        /// <summary>
-        /// Whether or not the given button has been released.
-        /// </summary>
-        /// <param name="button">The given button.</param>
-        bool IsButtonReleased(string button);
+        /// </inheritdoc>
+        public bool IsButtonPressed(string button)
+        {
+            Raylib_cs.MouseButton raylibButton = buttons[button.ToLower()];
+            return Raylib.IsMouseButtonPressed(raylibButton);
+        }
 
-        /// <summary>
-        /// Whether or not the given button is up.
-        /// </summary>
-        /// <param name="button">The given button.</param>
-        bool IsButtonUp(string button);
+        /// </inheritdoc>
+        public bool IsButtonReleased(string button)
+        {
+            Raylib_cs.MouseButton raylibButton = buttons[button.ToLower()];
+            return Raylib.IsMouseButtonReleased(raylibButton);
+        }
+
+        /// </inheritdoc>
+        public bool IsButtonUp(string button)
+        {
+            Raylib_cs.MouseButton raylibButton = buttons[button.ToLower()];
+            return Raylib.IsMouseButtonUp(raylibButton);
+        }
     }
 }
