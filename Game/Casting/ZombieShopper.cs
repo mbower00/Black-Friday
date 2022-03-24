6dc1820e49209bc8c57d5bc4a25f056e9a606f8a
Mitchell Bower <mitchell@bowermail.net>
2022-03-23 18:33:55 -0600

    added a "frame tick" counter to the ZombieShopper

--- Game/Casting/ZombieShopper.cs
+++ Game/Casting/ZombieShopper.cs
@@ -1,7 +1,32 @@
 namespace black_friday.Game.Casting{
-    class ZombieShoppers : Actor{
-        public ZombieShoppers(){
+    class ZombieShopper : Actor{
+        private int frameTick;
+        
+        public ZombieShopper(){
+            this.frameTick = 0;
+        }
 
+        /// <summary>
+        /// Gets the frame tick for this instance of ZombieShopper
+        /// </summary>
+        /// <returns>The frame tick</returns>
+        int GetFrameTick(){
+            return this.frameTick;
+        }
+
+        /// <summary>
+        /// Increments the frame tick
+        /// </summary>
+        /// <param name="increment">The amount by which to increment the frame tick (DEFAULT = 1)</param>
+        void IncrementFrameTick(int increment = 1){
+            this.frameTick += increment;
+        }
+        
+        /// <summary>
+        /// Resets the frame tick to 0
+        /// </summary>
+        void ResetFrameTick(){
+            this.frameTick = 0;
         }
     }
 
